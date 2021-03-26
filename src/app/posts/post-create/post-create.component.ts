20caaf432a19829988b6728f6b73931cc5506855
Raphael Vareilles <raphael.vareilles@gmail.com>
2021-03-26 14:50:17 +0100

    first commit

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.scss']
+})
+export class PostCreateComponent implements OnInit {
+  newpost = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddPost() {
+    this.newpost = 'bwaaaah!';
+  }
+
+}
